5a746ee95612d9eb0598f83483dceddc476ea32e
celestesmthomas <celestesmthomas@gmail.com>
2020-04-22 20:57:01 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
+
+
+def my_collect(array)
+    collection =[]
+    index = 0
+     while index < array.length  
+collection << yield(array[index])
+index += 1
+    end
+    collection
+end
+
 
 
